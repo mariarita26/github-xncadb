804e04b11b7e83b056d80d38bb4bff3e2f2fb2df
mariarita26 <89050107+mariarita26@users.noreply.github.com>
2022-10-06 11:20:04 -0300

    1 file modified, 1 added, 1 deleted

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,8 +5,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 
-
-
 @NgModule({
   imports: [
     CommonModule,
