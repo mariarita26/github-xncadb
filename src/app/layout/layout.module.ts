c6ab6ce050ff2e3660e015f19881514ceb93c868
mariarita26 <89050107+mariarita26@users.noreply.github.com>
2022-10-06 11:00:58 -0300

    2 files modified, 4 added

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    MatButtonModule,
+    MatMenuModule
+  ],
+  exports: [MenuComponent],
+  declarations: [
+    MenuComponent
+  ] 
+})
+export class LayoutModule { }
